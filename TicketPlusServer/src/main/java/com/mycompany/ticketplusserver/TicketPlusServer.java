fab8e85a54a5a2df3bdc5d39f43335a8ecbf4b18
David <David@DavidPC>
2026-03-13 18:02:21 +0100

    Modificación main para distinción.

---
+++ TicketPlusServer/src/main/java/com/mycompany/ticketplusserver/TicketPlusServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ticketplusserver;
+
+/**
+ *
+ * @author David
+ */
+public class TicketPlusServer {
+
+    public static void main(String[] args) {
+        System.out.println("Clase main para el ticketplus server!");
+    }
+}
