25be11c3ca599f73d812e2722a531689088dd9d2
David <David@DavidPC>
2026-03-15 18:39:38 +0100

    Añadido de clase ServerAplication, fichero .propierties i nfactions.xml

---
+++ TicketPlusServer/src/main/java/com/ticketingmaster/ticketplusserver/TicketPlusServerApplication.java
@@ -0,0 +1,13 @@
+package com.ticketingmaster.ticketplusserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPlusServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketPlusServerApplication.class, args);
+    }
+
+}
